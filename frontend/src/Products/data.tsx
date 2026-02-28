c82addc534bb4f2789a965c3f07c4a2579ce609a
John Tressel <jtressel30@gmail.com>
2026-02-28 15:15:01 -0500

    Added the catalog features

---
+++ frontend/src/Products/data.tsx
@@ -0,0 +1,34 @@
+export default {
+    products: [{
+        name: 'Shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    },
+    {
+        name: 'Shirt 2',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    },
+    {
+        name: 'Shirt 3',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    },
+    {
+        name: 'Shirt 4',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    }]
+}
